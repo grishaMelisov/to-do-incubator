73d80aea7a325b0926ba9a370ae08e48d6c0c49a
redrum <urecherrydream@gmail.com>
2022-08-27 22:54:15 +0300

    corrected filtration

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
 import React, { useState } from 'react'
 import './App.css'
 import Todo, { TasksArrayType } from './Components/Todolist'
+export type FilterTypes = 'all' | 'completed' | 'active'
 
 function App() {
-  const arrOfData = [
+  const [tasks, setTasks] = useState<Array<TasksArrayType>>([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
-  ]
+  ])
 
-  const [tasks, setTasks] = useState<Array<TasksArrayType>>(arrOfData)
+  const [filter, setFilter] = useState<FilterTypes>('all')
 
-  function activeTasks(activator: 'all' | 'active' | 'completed') {
-    if (activator === 'completed') {
-      return setTasks(arrOfData.filter((item) => item.isDone))
-    }
-    if (activator === 'active') {
-      return setTasks(arrOfData.filter((item) => !item.isDone))
-    }
-    return setTasks(arrOfData)
+  let filtratedTasks = tasks
+  if (filter === 'completed') {
+    filtratedTasks = tasks.filter((item) => item.isDone)
+  }
+  if (filter === 'active') {
+    filtratedTasks = tasks.filter((item) => !item.isDone)
+  }
+  if (filter === 'all') {
+    filtratedTasks = tasks
+  }
+
+  function toFilter(value: FilterTypes) {
+    setFilter(value)
+  }
+
+  function deleteTask(id: number) {
+    setTasks(tasks.filter((item) => item.id !== id))
   }
 
   return (
     <div className='App'>
-      <Todo title={'This week'} tasks={tasks} active={activeTasks} />
+      <Todo
+        title={'This week'}
+        tasks={filtratedTasks}
+        filter={toFilter}
+        delete={deleteTask}
+      />
     </div>
   )
 }
--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FilterTypes } from '../App'
 
 type TodoPropsType = {
   title: string
   tasks: Array<TasksArrayType>
-  active: (activator: 'all' | 'active' | 'completed') => void
+  filter: (value: FilterTypes) => void
+  delete: (id: number) => void
 }
 
 export type TasksArrayType = {
@@ -26,14 +28,15 @@ function Todo(props: TodoPropsType) {
             <li key={item.id}>
               <input type='checkbox' checked={item.isDone} />
               <span>{item.title}</span>
+              <button onClick={() => props.delete(item.id)}>x</button>
             </li>
           )
         })}
       </ul>
       <div>
-        <button onClick={() => props.active('all')}>All</button>
-        <button onClick={() => props.active('active')}>Active</button>
-        <button onClick={() => props.active('completed')}>Completed</button>
+        <button onClick={() => props.filter('all')}>All</button>
+        <button onClick={() => props.filter('active')}>Active</button>
+        <button onClick={() => props.filter('completed')}>Completed</button>
       </div>
     </div>
   )
